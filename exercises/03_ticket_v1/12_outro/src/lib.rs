9a18a6cffa4c7e2dd5c20c4eb02086bae79914ac
EricZzz <biggogten@gmail.com>
2025-08-27 21:26:05 +0800

    complete 03_ticket exercises

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -3,7 +3,7 @@
 //   The product name can't be empty and it can't be longer than 300 bytes.
 //   The quantity must be strictly greater than zero.
 //   The unit price is in cents and must be strictly greater than zero.
-//   Order must include a method named `total` that returns the total price of the order.
+//   Order must inßclude a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
 //
 // Tests are located in a different place this time—in the `tests` folder.
@@ -11,3 +11,58 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty");
+        }
+        if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes");
+        }
+        if quantity == 0 {
+            panic!("Quantity must be strictly greater than zero");
+        }
+        if unit_price == 0 {
+            panic!("Unit price must be strictly greater than zero");
+        }
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> u32 {
+        self.quantity
+    }
+
+    pub fn unit_price(&self) -> u32 {
+        self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+}
